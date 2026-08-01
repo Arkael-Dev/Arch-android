41f57bc53c242c66d13644093664b6386c20cf33
Arkaelkernel <151031855+Arkael-Dev@users.noreply.github.com>
2026-08-01 09:48:51 +0700

    initial commit

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # ============================================================
-# Droidspaces + Termux:X11 + PulseAudio + XFCE Setup
-# Container: arkael
+# Droidspaces + Termux:X11 + PulseAudio + gnome Setup
+# Container: ark
 # Display: :5
 # ============================================================
 
 # 1. Force-stop Termux:X11, PulseAudio, and stop the container
 su -c "am force-stop com.termux.x11" 2>/dev/null
 
-su -c "/data/local/Droidspaces/bin/droidspaces --name='arkael' stop" 2>/dev/null
+su -c "/data/local/Droidspaces/bin/droidspaces --name='ark' stop" 2>/dev/null
 
 pkill -9 termux-x11 Xorg virglrenderer pulseaudio 2>/dev/null
 
@@ -33,13 +33,13 @@ chmod 777 /data/data/com.termux/files/usr/tmp/.X11-unix
 # 3. FIX AUDIO:
 # Copy the PulseAudio cookie using root access
 # to avoid Permission Denied errors
-su -c "mkdir -p /mnt/Droidspaces/arkael/root/.config/pulse"
+su -c "mkdir -p /mnt/Droidspaces/ark/root/.config/pulse"
 
 su -c "cp /data/data/com.termux/files/home/.config/pulse/cookie \
-/mnt/Droidspaces/arkael/root/.config/pulse/cookie 2>/dev/null"
+/mnt/Droidspaces/ark/root/.config/pulse/cookie 2>/dev/null"
 
 su -c "cp /data/data/com.termux/files/usr/etc/pulse/cookie \
-/mnt/Droidspaces/arkael/root/.config/pulse/cookie 2>/dev/null"
+/mnt/Droidspaces/ark/root/.config/pulse/cookie 2>/dev/null"
 
 
 # 4. Start Termux:X11 on display :5 silently
@@ -49,13 +49,13 @@ sleep 5
 
 
 # 5. Start the Droidspaces container
-su -c "/data/local/Droidspaces/bin/droidspaces --name='arkael' start"
+su -c "/data/local/Droidspaces/bin/droidspaces --name='ark' start"
 
 sleep 3
 
 
 # 6. Add the required commands to the container's .bashrc
-# Includes DBus, zombie-process cleanup, XFCE cleanup,
+# Includes DBus, zombie-process cleanup, gnome cleanup,
 # and GPU acceleration configuration
 su -c "echo 'export DISPLAY=:5; \
 export XDG_RUNTIME_DIR=/run/user/0; \
@@ -64,15 +64,15 @@ chmod 700 \$XDG_RUNTIME_DIR; \
 export NO_AT_BRIDGE=1; \
 export GALLIUM_DRIVER=virpipe; \
 pkill -9 pulseaudio 2>/dev/null; \
-pkill -9 -f xfce 2>/dev/null; \
-pkill -9 xfwm4 xfdesktop xfce4-panel xscreensaver 2>/dev/null; \
+pkill -9 -f gnome 2>/dev/null; \
+pkill -9 gnome-shell gnome-session gsd-* 2>/dev/null; \
 sleep 1; \
 rm -rf /tmp/.Xauthority; \
-dbus-launch startxfce4; \
-sed -i \"/startxfce4/d\" /root/.bashrc' \
->> /mnt/Droidspaces/arkael/root/.bashrc"
+dbus-launch gnome-session; \
+sed -i \"/gnome-session/d\" /root/.bashrc' \
+>> /mnt/Droidspaces/ark/root/.bashrc"
 
 
 # 7. Enter the container
 # GUI, audio, and GPU fixes will run automatically
-su -c "/data/local/Droidspaces/bin/droidspaces --name='arkael' enter"
+su -c "/data/local/Droidspaces/bin/droidspaces --name='ark' enter"
